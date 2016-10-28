56ded888ed845956c40b46b6fd0a75a9be52d399
fanch <fanch@fracanet.fr>
2016-10-28 11:27:42 +0200

    Mise en place de l'authentification des users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :authenticate_user_if_not_in_admin
+
+  def authenticate_user_if_not_in_admin
+    authenticate_user! unless self.class.parent == RailsAdmin
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,10 @@ Admin.create!(
   password:               'admin888',
   password_confirmation:  'admin888'
 )
+
+# User default
+User.create!(
+  email:                  'user@user.com',
+  password:               'user8888',
+  password_confirmation:  'user8888'
+)
