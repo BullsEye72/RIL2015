2fc599cae5b313f31518cc0a9f789a69eeddd880
Alex <alex.m72000@hotmail.fr>
2016-11-29 15:52:20 +0000

    Init controller+routes REST pour les clients, génération des vues stub

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'clients/new'
+
+  get 'clients/create'
+
+  get 'clients/update'
+
+  get 'clients/edit'
+
+  get 'clients/destroy'
+
+  get 'clients/index'
+
+  get 'clients/show'
+
   # USERS & ADMINISTRATION
   devise_for :admins
   devise_for :users
