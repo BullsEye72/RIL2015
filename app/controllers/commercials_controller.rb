249fba6c804eec0cd3a94c33e721be72706fd376
fanch <fanch@fracanet.fr>
2016-12-06 22:13:56 +0100

    Mise en place d'authorisation pour les commerciaux

--- app/controllers/commercials_controller.rb
+++ app/controllers/commercials_controller.rb
@@ -1,4 +1,7 @@
 class CommercialsController < ApplicationController
+  authorize_resource :class => false
+
   def index
   end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 
   def index
+    redirect_to commercials_url if current_user.has_role? :commercial
   end
 end
