01e0f5d3dba5892376d0398f884baa297abd1b9b
fanch <fanch@fracanet.fr>
2016-12-07 23:21:25 +0100

    Affichage des erreurs d'autorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user_if_not_in_admin
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   def authenticate_user_if_not_in_admin
     authenticate_user! unless self.class.parent == RailsAdmin
   end
