1209c71a845842f5fc2866fd3d95ae0b048cab76
Anthony Jaspers <jaspers.anthony@gmail.com>
2016-12-15 09:58:46 +0000

    Logos + Dynamique commercial menu + corrections divers

--- app/controllers/commercials_controller.rb
+++ app/controllers/commercials_controller.rb
@@ -4,35 +4,24 @@ class CommercialsController < ApplicationController
   
   def index
     
-    @param_page = {name: "page", value: params[:page]}
-    @param_sort = {name: "sort", value: params[:sort]}
+    @param_page = {name: 'page', value: params[:page]}
+    @param_sort = {name: 'sort', value: params[:sort]}
+    
+    @projects_menu = Project.order("updated_at DESC").first(5)
+    
+    order = (['1','3','6'].include? params[:sort]) ? 'ASC' : 'DESC'
     
     case params[:sort]
-    when '1'
-      @projects = Project.joins(:customer).order('customers.lastname DESC').page @param_page[:value]
-    when '2'
-      @projects = Project.joins(:customer).order('customers.lastname ASC').page @param_page[:value]
-    when '3'
-      @projects = Project.order('id ASC').page @param_page[:value]
-    when '4'
-      @projects = Project.order('id DESC').page @param_page[:value]
-    when '5'
-      @projects = Project.order('updated_at DESC').page @param_page[:value]
-    when '6'
-      @projects = Project.order('updated_at ASC').page @param_page[:value]
+    when '1','2'
+      @projects = Project.joins(:customer).order("customers.lastname #{order}")
+    when '3','4'
+      @projects = Project.order("id  #{order}")
+    when '5','6'
+      @projects = Project.order("updated_at #{order}")
     else
-      @projects = Project.joins(:customer).order('customers.lastname DESC').page @param_page[:value]
+      @projects = Project.joins(:customer).order('customers.lastname ASC')
     end
-    
-    
-    
-  end
-  
-  helper_method :get_params
-  def get_params
-      return "?#{@param_sort[:name]}=" if params[:page]==nil
-      return "?#{@param_page[:name]}=#{@param_page[:value]}&#{@param_sort[:name]}=" if params[:page]!=nil
-      return ''
-  end
 
+    @projects = @projects.page @param_page[:value]
+  end
 end
