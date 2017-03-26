ece8715c72a72443f0b40460432ae248d6af50a0
fanch <fanch@fracanet.fr>
2017-03-26 15:16:52 +0200

    Mise en place de validations sur le changement d'état d'un produit

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,6 +19,12 @@
 #
 
 class Product < ActiveRecord::Base
+
+  @@invalid_product_state = ProductState.find_by_name('invalide')
+  @@default_product_state = ProductState.find_by_name('defaut')
+  @@normal_product_state = ProductState.find_by_name('normal')
+  @@draft_product_state = ProductState.find_by_name('brouillon')
+
   before_create :set_product_on_create
 
   belongs_to :drawing
@@ -27,14 +33,17 @@ class Product < ActiveRecord::Base
   has_many :modules_products
   has_many :house_modules, through: :modules_products
 
-  scope :default, -> { where(product_state: ProductState.find_by_name('defaut'))}
-  scope :draft, -> { where(product_state: ProductState.find_by_name('brouillon'))}
+  scope :default, -> { where(product_state: @default_product_state)}
+  scope :draft, -> { where(product_state: @draft_product_state)}
   scope :draft_or_default, -> {
     where(product_state: [
         ProductState.find_by_name('brouillon'),
         ProductState.find_by_name('defaut'),
     ])
   }
+
+  validate :only_draft_can_be_invalidate
+  validate :normal_and_default_can_t_be_draft
 
   def product_price
     price = 0
@@ -50,6 +59,20 @@ class Product < ActiveRecord::Base
 
   def set_product_on_create
     self.product_state ||= ProductState.find_by_name('brouillon')
+  end
+
+  private
+
+  def only_draft_can_be_invalidate
+    if (product_state == @@invalid_product_state ) and (product_state_id_was != @@draft_product_state.id)
+      errors.add(:product_state, 'Seul les brouillons peuvent être invalidé')
+    end
+  end
+
+  def normal_and_default_can_t_be_draft
+    if (product_state == @@draft_product_state) and (product_state_id_was.in? [@@normal_product_state.id, @@default_product_state.id])
+      errors.add(:product_state, 'Les produits normaux ou par défaut ne peuvent devenir des brouillons')
+    end
   end
 
 end
